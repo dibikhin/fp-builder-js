9226711cd13a2a28c2857436fc5889e6b2758a4d
Roman Dibikhin <dibikhin@gmail.com>
2020-06-11 00:51:30 +0300

    wip

--- generic_evolver.js
+++ generic_evolver.js
@@ -9,12 +9,13 @@ const {
 } = require('ramda')
 
 /**
- * Evolves an object by merging with diff
+ * Evolves an object by merging with diff.
+ * Makes possible to call Builder's steps in any order
  *
  * @param {object} params
  * @param {object} params.before - a target object to evolve
  * @param {object} params.diff - a diff for evolving the target object
- * @returns {object} - evolved object
+ * @returns {object} evolved object
  */
 function evolve({ before, diff, }) {
     const merged = mergeDeepRight(before, diff)
